e67102d580868e1873afea5124e511f0e77e90fb
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-23 21:14:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
-  my_each(array) do |element| 
-    
-    
+  
+  i = 0
+  
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
 end
